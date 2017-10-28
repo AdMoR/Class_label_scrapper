954585fff8932a6585ea870def7e19d126de9866
amor <adrien_morvan@hotmail.fr>
2017-10-28 17:52:55 +0200

    Modification on path handling

--- crawler.py
+++ crawler.py
@@ -13,12 +13,14 @@ class CDiscountImageCrawler(object):
 
     def parse_class_file(self):
         for i, line in enumerate(open(self.class_file)):
+            if i == 0:
+                continue
             tokens = line.split(',')
-            self.classes[int(tokens[0])] = tokens[-1]
+            self.classes[int(tokens[0])] = tokens[-1][:-1]
 
     def crawl_keyword(self, classe_index):
         keyword = self.classes[classe_index]
-        image_directory = '/Users/amorvan/Documents/Databases/image_{}'.format(keyword)
+        image_directory = '/home/amor/Documents/databases/image_{}'.format(keyword)
         self.classes_to_dir[classe_index] = image_directory
         if not os.path.exists(image_directory):
             os.mkdir(image_directory)
@@ -31,12 +33,10 @@ class CDiscountImageCrawler(object):
                              max_size=None)
 
     def run(self):
-        for i, keyword in self.classes.items():
-            self.crawl_keyword(keyword)
-            if i > 30:
-                break
+        for img_class in self.classes.keys():
+            self.crawl_keyword(img_class)
 
-config_file = "/Users/amorvan/Documents/code_dw/web_image_crawler/category_names.csv"
+config_file = "./category_names.csv"
 c_crawler = CDiscountImageCrawler(config_file)
 c_crawler.run()
 json.dumps(c_crawler.classes_to_dir, open("classes_to_dir.json", "w"))
